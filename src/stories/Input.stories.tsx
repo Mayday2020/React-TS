70c8bc7fcee6089d9f14a5b01cd963e4b972c718
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-07-08 09:24:36 +0300

    'less13storybook'

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -30,4 +30,30 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
       <button onClick={ save }> save </button> - {value}
     </div>
 }
+export const ControlledInput = () => {
+  let [parentValue, setParentValue] = useState("")
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(event.currentTarget.value)
+  }
+  return <input value={parentValue} type="mail" onChange={ onChange }/>
+}
+export const ControlledCheckbox = () => {
+  let [parentValue, setParentValue] = useState(true)
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(event.currentTarget.checked)
+  }
+  return <input checked={parentValue} type="checkbox" onChange={ onChange }/>
+}
+export const ControlledSelect = () => {
+  let [parentValue, setParentValue] = useState<string | undefined>('1')
+  const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(event.currentTarget.value)
+  }
+  return <select value={parentValue} onChange={ onChange }>
+    <option>none</option>
+    <option value="1">Kiev</option>
+    <option value="2">Moscow</option>
+    <option value="3">Minsk</option>
+  </select>
+}
 
