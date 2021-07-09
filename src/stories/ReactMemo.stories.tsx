94c4340097a097c9ab601624fec2c7eb24b0c690
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-07-10 00:42:45 +0300

    'less16react.memo'

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+export default {
+    title: 'React.memo demo'
+}
+
+const NewMessagesCounter = (props: { count: number }) => {
+    console.log('count')
+    return <div>{props.count}</div>
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('users')
+    return <div>
+        {props.users.map((u, i)=> (<div key={i}>{u}</div>))}
+    </div>
+}
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem'])
+    return <div>
+        <button onClick={()=> setCounter(counter + 1)}>+</button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
+    </div>
+}
