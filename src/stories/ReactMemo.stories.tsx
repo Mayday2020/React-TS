342c731c471f86673a8f7e6498e36a76e59c263d
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-08-17 18:02:53 +0300

    'less17useMemo'

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -5,12 +5,10 @@ export default {
 }
 
 const NewMessagesCounter = (props: { count: number }) => {
-    console.log('count')
     return <div>{props.count}</div>
 }
 
 const UsersSecret = (props: { users: Array<string> }) => {
-    console.log('users')
     return <div>
         {props.users.map((u, i)=> (<div key={i}>{u}</div>))}
     </div>
@@ -19,9 +17,15 @@ const Users = React.memo(UsersSecret)
 
 export const Example1 = () => {
     const [counter, setCounter] = useState(0)
-    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem'])
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Katya'])
+
+    const addUser =() => {
+        const newUsers = [...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUsers)
+    }
     return <div>
         <button onClick={()=> setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}>add User</button>
         <NewMessagesCounter count={counter}/>
         <Users users={users}/>
     </div>
