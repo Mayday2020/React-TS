f7feb627a4e37c39bc553d7badb93646ba0babc1
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-08-18 09:09:05 +0300

    'less21useEffect'

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -30,3 +30,24 @@ export const SimpleExample = () => {
         </>
 };
 
+export const SimpleExample1 = () => {
+    console.log('SimpleExample')
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+    console.log('SimpleExample done!')
+    useEffect(()=>{
+        setInterval(()=>{
+            console.log('setInterval done!')
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+
+    return <>
+        Hello, count = {counter}  | fake = {fake}
+        {/*<button onClick={ () => { setCounter(counter +1) } }> + count </button>
+        <button onClick={ () => { setFake(fake +1) } }> + fake </button>*/}
+    </>
+};
+
+
